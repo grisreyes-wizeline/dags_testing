588c8e83a02f29dd4b80e64962e693e9388e131d
grisreyes-wizeline <92352894+grisreyes-wizeline@users.noreply.github.com>
2023-09-19 04:03:18 -0600

    Update upload_to_gcs_2.py

--- dags/upload_to_gcs_2.py
+++ dags/upload_to_gcs_2.py
@@ -9,14 +9,10 @@ import os
 from datetime import timedelta
 from datetime import datetime
 
-bucket_name = 'africa-deb-bucket'  # Your GCS bucket name
-gcs_conn_id = 'google_cloud_storage'
-local_file_path = '/Users/grisell.reyes/Google-Africa-DEB/session_06/resources/local_repository_file/warehouse_and_retail_sales.csv'
-
 def upload_to_gcs():
-    bucket_name = bucket_name  # Your GCS bucket name
+    bucket_name = 'africa-deb-bucket'  
     gcs_conn_id = 'google_cloud_storage'
-    local_file_path = local_file_path
+    local_file_path = '/Users/grisell.reyes/Google-Africa-DEB/session_06/resources/local_repository_file/warehouse_and_retail_sales.csv'
     upload_task = LocalFilesystemToGCSOperator(
         task_id=f'upload_to_gcs',
         src=local_file_path,
