5a10c81eb607e1a4355e13aaf56a30b7b74fb948
grisreyes-wizeline <92352894+grisreyes-wizeline@users.noreply.github.com>
2023-09-19 02:09:21 -0600

    Update upload_to_gcs.py

--- dags/upload_to_gcs.py
+++ dags/upload_to_gcs.py
@@ -1,1 +1,37 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.contrib.sensors import file_sensor
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.contrib.operators.file_to_gcs import FileToGoogleCloudStorageOperator
+import convert_json_to_csv
 
+seven_days_ago = datetime.combine(datetime.today() - timedelta(1),
+                                  datetime.min.time())
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': seven_days_ago,
+    'email': ['grisell.reyes@wizeline.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG('warehouse-retail-sales-dag', default_args=default_args, schedule_interval='@daily')
+
+
+t1 = DummyOperator(task_id='op1', dag=dag)
+
+t2 = FileToGoogleCloudStorageOperator(
+    task_id='fileToGCS',
+    src='/Users/grisell.reyes/Google-Africa-DEB/session_06/resources/local_repository_file/warehouse_and_retail_sales.csv',
+    dst='data/warehouse_and_retail_sales_bucket.csv',
+    bucket='africa-deb-bucket',
+    google_cloud_storage_conn_id='google_cloud_default',
+    dag=dag
+)
+
+t1 >> t2
