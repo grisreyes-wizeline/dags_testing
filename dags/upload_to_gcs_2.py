b67f2fe918b428a682607d10586ac7d8e9ddd878
grisreyes-wizeline <92352894+grisreyes-wizeline@users.noreply.github.com>
2023-09-19 03:48:15 -0600

    Update upload_to_gcs_2.py

--- dags/upload_to_gcs_2.py
+++ dags/upload_to_gcs_2.py
@@ -9,19 +9,9 @@ import os
 from datetime import timedelta
 from datetime import datetime
 
-# Define the Python function to upload files to GCS
-def upload_to_gcs():
-    bucket_name = 'africa-deb-bucket'  # Your GCS bucket name
-    gcs_conn_id = 'google_cloud_storage'
-    local_file_path = '/Users/grisell.reyes/Google-Africa-DEB/session_06/resources/local_repository_file/warehouse_and_retail_sales.csv'
-    localToGCS1 = FileToGoogleCloudStorageOperator(
-        task_id='LocalToGCS',
-        src= local_file_path,
-        dst='warehouse_and_retail_sales_2.csv',
-        bucket= bucket_name,
-        google_cloud_storage_conn_id='google_cloud_storage',
-        dag=dag
-        )
+bucket_name = 'africa-deb-bucket'  # Your GCS bucket name
+gcs_conn_id = 'google_cloud_storage'
+local_file_path = '/Users/grisell.reyes/Google-Africa-DEB/session_06/resources/local_repository_file/warehouse_and_retail_sales.csv'
 
 default_args = {
     'owner': 'grisell.reyes',
@@ -34,17 +24,20 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 # Define your DAG
-with DAG('RecipeWarehouse', schedule_interval='@once', default_args=default_args) as dag:
+with DAG('uplod_to_gcs_2', schedule_interval='@once', default_args=default_args) as dag:
     start_pipeline = DummyOperator(
         task_id = 'start_pipeline',
         dag = dag
         )
         
-    upload_to_gcs = PythonOperator(
-        task_id='upload_to_gcs',
-        python_callable=upload_to_gcs,
-        dag=dag  # Assign the DAG to the task
+    local_to_gcs = FileToGoogleCloudStorageOperator(
+        task_id='LocalToGCS',
+        src= local_file_path,
+        dst='warehouse_and_retail_sales_2.csv',
+        bucket= bucket_name,
+        google_cloud_storage_conn_id='gcs_conn_id',
+        dag=dag
         )
 
 # Define your DAG dependencies
-start_pipeline >> upload_to_gcs
+start_pipeline >> local_to_gcs
