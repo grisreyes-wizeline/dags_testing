45b69a5516dd04166e697a45766ebd2b557fc873
grisreyes-wizeline <92352894+grisreyes-wizeline@users.noreply.github.com>
2023-09-19 05:43:21 -0600

    Update ingest_from_gcs_to_postgres.py

--- dags/ingest_from_gcs_to_postgres.py
+++ dags/ingest_from_gcs_to_postgres.py
@@ -79,8 +79,8 @@ with DAG(
         postgres_conn_id=POSTGRES_CONN_ID,
         sql=f"""
             CREATE TABLE IF NOT EXISTS {POSTGRES_TABLE_NAME} (
-                YEAR INTEGER,
-                MONTH INTEGER,
+                YEAR NUMERIC,
+                MONTH NUMERIC,
                 SUPPLIER TEXT,
                 ITEM_CODE TEXT,
                 ITEM_DESCRIPTION TEXT,
